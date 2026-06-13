cf63f6fa78a38184f4c4ae45f39f88b2aab79c18
Kenta Kubo <601636+kkebo@users.noreply.github.com>
2026-06-14 02:27:38 +0900

    Make Package.swift as small as possible

--- Package.swift
+++ Package.swift
@@ -1,35 +1,11 @@
 // swift-tools-version: 6.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "dependabot-swift-example",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "dependabot-swift-example",
-            targets: ["dependabot_swift_example"]
-        ),
-    ],
     dependencies: [
         .package(url: "https://github.com/ordo-one/benchmark", from: "1.30.0", traits: []),
     ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "dependabot_swift_example",
-            swiftSettings: [
-                .enableUpcomingFeature("ApproachableConcurrency"),
-            ],
-        ),
-        .testTarget(
-            name: "dependabot_swift_exampleTests",
-            dependencies: ["dependabot_swift_example"],
-            swiftSettings: [
-                .enableUpcomingFeature("ApproachableConcurrency"),
-            ],
-        ),
-    ]
+    targets: []
 )
